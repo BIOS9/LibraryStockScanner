5ff46faf69cf76b9808e034c69615acf78b25b90
NightFish <nightfish.net@gmail.com>
2021-07-01 19:26:21 +1200

    Implement reader response and add tests.

--- src/RfidAssetReader3M/ReaderCommunication/ReaderResponse.cs
+++ src/RfidAssetReader3M/ReaderCommunication/ReaderResponse.cs
@@ -6,10 +6,125 @@
 
 namespace RfidAssetReader3M.ReaderCommunication
 {
+    using System;
+    using System.Data;
+
     /// <summary>
-    /// A response to a command send back from the reader.
+    /// A command response sent back from the reader.
     /// </summary>
     internal class ReaderResponse
     {
+        private readonly byte[] fullResponse;
+        private readonly byte[] response;
+        private readonly byte[] checksum;
+        private readonly CommunicationType communicationType;
+        private readonly bool validChecksum;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ReaderResponse"/> class.
+        /// </summary>
+        /// <param name="fullResponse">Full response from the reader including checksum and header.</param>
+        /// <param name="ignoreChecksum">Allows continuing with an invalid checksum.</param>
+        public ReaderResponse(byte[] fullResponse, bool ignoreChecksum = false)
+        {
+            this.fullResponse = fullResponse ?? throw new ArgumentNullException(nameof(fullResponse));
+
+            // Ensure full response has at least 3 bytes for the header and 2 bytes for the checksum.
+            if (fullResponse.Length < 5)
+            {
+                throw new ArgumentException("Full response must contain at least 5 bytes.", nameof(fullResponse));
+            }
+
+            // Ensure full response is not longer than the maximum size.
+            // Max size is governed by the max data length (255) + the header (3)
+            if (fullResponse.Length > 258)
+            {
+                throw new ArgumentException("Full response is too long.");
+            }
+
+            // Ensure communication type is an expected value.
+            if (!Enum.IsDefined(typeof(CommunicationType), fullResponse[0]))
+            {
+                throw new ArgumentException("Unknown communication type.", nameof(fullResponse));
+            }
+            else
+            {
+                this.communicationType = (CommunicationType)fullResponse[0];
+            }
+
+            // Ensure that weird space byte is 0 until I figure out what it does.
+            if (fullResponse[1] != 0)
+            {
+                throw new ArgumentException("Unexpected value for byte 2.", nameof(fullResponse));
+            }
+
+            byte dataLength = fullResponse[2];
+
+            // Ensure data length is at least 2 since it includes the checksum.
+            if (dataLength < 2)
+            {
+                throw new ArgumentException("Data length is less than 2. 2 bytes are required to fit the checksum.", nameof(fullResponse));
+            }
+
+            // Ensure length value matches length of given payload.
+            if (dataLength != fullResponse.Length - 3)
+            {
+                throw new ArgumentException("Data length value does not match the length of the actual data.", nameof(fullResponse));
+            }
+
+            // Allocate and populate response data.
+            this.response = new byte[dataLength - 2];
+            Array.Copy(fullResponse, 3, this.response, 0, dataLength - 2);
+
+            // Allocate and populate checksum.
+            this.checksum = new byte[2];
+            Array.Copy(fullResponse, fullResponse.Length - 2, this.checksum, 0, 2);
+
+            // Calculate and validate checksum.
+            this.validChecksum = this.ValidateChecksum();
+            if (!ignoreChecksum && !this.validChecksum)
+            {
+                throw new DataException("Invalid checksum");
+            }
+        }
+
+        /// <summary>
+        /// Gets the communication type header byte.
+        /// </summary>
+        public CommunicationType CommunicationType => this.communicationType;
+
+        /// <summary>
+        /// Gets the raw command to send to the reader.
+        /// This does not include any of the header bytes or the checksum.
+        /// </summary>
+        public ReadOnlySpan<byte> Response => this.response;
+
+        /// <summary>
+        /// Gets the modified CRC16-CCITT checksum of the data.
+        /// This checksum matches the format expected by the 3M RFID reader.
+        /// </summary>
+        public ReadOnlySpan<byte> Checksum => this.checksum;
+
+        /// <summary>
+        /// Gets the full response from the reader including checksum and header.
+        /// </summary>
+        public ReadOnlySpan<byte> FullResponse => this.fullResponse;
+
+        /// <summary>
+        /// Gets a value indicating whether the provided checksum matches the calcluated
+        /// checksum of the data.
+        /// </summary>
+        public bool IsChecksumValid => this.validChecksum;
+
+        private bool ValidateChecksum()
+        {
+            // Calculate checksum of the command.
+            Span<byte> checksumData = this.fullResponse;
+            checksumData = checksumData.Slice(1); // Skip the communication type.
+            checksumData = checksumData.Slice(0, checksumData.Length - 2); // Skip final 2 checksum bytes.
+            Span<byte> checksumResult = BitConverter.GetBytes(Helpers.Checksum.CalculateCrc16(checksumData));
+            checksumResult.Reverse(); // Swap endian
+            return checksumResult.SequenceEqual(this.checksum);
+        }
     }
 }
---
+++ src/RfidAssetReader3MTests/ReaderResponseTests.cs
@@ -0,0 +1,175 @@
+﻿namespace RfidAssetReader3MTests
+{
+    using NUnit.Framework;
+    using RfidAssetReader3M.ReaderCommunication;
+    using System;
+    using System.Collections.Generic;
+    using System.Data;
+    using System.Text;
+
+    public class ReaderResponseTests
+    {
+        struct ChecksumTestItem
+        {
+            public byte[] response;
+            public byte[] expectedChecksum;
+
+            public ChecksumTestItem(byte[] response, byte[] expectedChecksum)
+            {
+                this.response = response;
+                this.expectedChecksum = expectedChecksum;
+            }
+        }
+
+        List<ChecksumTestItem> testData = new List<ChecksumTestItem>
+        {
+            new ChecksumTestItem(
+                new byte[] { 0xFE, 0x00, 0x07 },
+                new byte[] { 0xDA, 0x02 }),
+            new ChecksumTestItem(
+                new byte[] { 0x04, 0x00, 0x11, 0x0a, 0x05, 0x00, 0x02 },
+                new byte[] { 0x72, 0x50 }),
+            new ChecksumTestItem(
+                new byte[] { 0xFE, 0x00, 0x00, 0x05, 0x00 },
+                new byte[] { 0xC9, 0x7B }),
+            new ChecksumTestItem(
+                new byte[] { 0xFE, 0x00, 0x00, 0x05, 0x01, 0xE0, 0x04, 0x01, 0x00, 0x31, 0x23, 0xAA, 0x26 },
+                new byte[] { 0x94, 0x1A }),
+            new ChecksumTestItem(
+                new byte[] { 0xFE, 0x00, 0x00, 0x05, 0x01, 0xE0, 0x04, 0x01, 0x00, 0x01, 0x7C, 0x0C, 0x38 },
+                new byte[] { 0x8E, 0x2B }),
+            new ChecksumTestItem(
+                new byte[] { 0xFE, 0x00, 0x00, 0x05, 0x01, 0xE0, 0x04, 0x01, 0x00, 0x31, 0x23, 0xAA, 0x28 },
+                new byte[] { 0x75, 0xD4 }),
+            new ChecksumTestItem(
+                new byte[] { 0xFE, 0x00, 0x00, 0x05, 0x02, 0xE0, 0x04, 0x01, 0x00, 0x31, 0x23, 0xAA, 0x26, 0xE0, 0x04, 0x01, 0x00, 0x01, 0x7C, 0x0C, 0x38 },
+                new byte[] { 0xCA, 0xDB }),
+            new ChecksumTestItem(
+                new byte[] { 0xFE, 0x00, 0x00, 0x05, 0x02, 0xE0, 0x04, 0x01, 0x00, 0x31, 0x23, 0xAA, 0x26, 0xE0, 0x04, 0x01, 0x00, 0x31, 0x23, 0xAA, 0x28 },
+                new byte[] { 0x31, 0x24 }),
+        };
+
+        [Test]
+        public void TestCommunicationType()
+        {
+            foreach (CommunicationType t in Enum.GetValues(typeof(CommunicationType)))
+            {
+                ReaderResponse rr = new ReaderResponse(new byte[] { (byte)t, 0x00, 0x05, 0xFE, 0x00, 0x07, 0xDA, 0x02 });
+                Assert.AreEqual(t, rr.CommunicationType);
+                Assert.AreEqual((byte)t, rr.FullResponse[0]);
+            }
+        }
+
+        [Test]
+        public void TestResponseLength()
+        {
+            for (int i = 0; i < 253; ++i)
+            {
+                for (int j = 0; j < 253; ++j)
+                {
+                    byte[] data = new byte[i + 5];
+                    data[0] = (byte)CommunicationType.Operation;
+                    data[2] = (byte)(j + 2);
+
+                    if (i == j)
+                    {
+                        ReaderResponse rr = new ReaderResponse(data, ignoreChecksum: true);
+                        Assert.AreEqual(data, rr.FullResponse.ToArray());
+                    }
+                    else
+                    {
+                        Assert.Throws<ArgumentException>(() =>
+                        {
+                            ReaderResponse rr = new ReaderResponse(data);
+                        });
+                    }
+                }
+            }
+
+            // Data too long.
+            Assert.Throws<ArgumentException>(() =>
+            {
+                new ReaderResponse(new byte[259], ignoreChecksum: true);
+            });
+        }
+
+        [Test]
+        public void TestResponseData()
+        {
+            for (int i = 5; i < 258; ++i)
+            {
+                Span<byte> data = new byte[i];
+                for (int j = 3; j < data.Length - 2; ++j)
+                {
+                    data[j] = (byte)j.GetHashCode(); // Just filling it with known stuff
+                }
+                data[0] = (byte)CommunicationType.Operation;
+                data[2] = (byte)(i - 3);
+                ReaderResponse rr = new ReaderResponse(data.ToArray(), ignoreChecksum: true);
+                Assert.AreEqual(data.Slice(3, i - 5).ToArray(), rr.Response.ToArray());
+            }
+        }
+
+        [Test]
+        public void TestChecksum()
+        {
+            foreach (ChecksumTestItem item in testData)
+            {
+                byte[] fullResponse = new byte[item.response.Length + 5];
+                fullResponse[0] = (byte)CommunicationType.Operation;
+                fullResponse[2] = (byte)(item.response.Length + 2);
+                Array.Copy(item.response, 0, fullResponse, 3, item.response.Length);
+                Array.Copy(item.expectedChecksum, 0, fullResponse, fullResponse.Length - 2, 2);
+
+                // Valid checksum
+                ReaderResponse rr = new ReaderResponse(fullResponse);
+                Assert.AreEqual(item.expectedChecksum, rr.Checksum.ToArray());
+                Assert.IsTrue(rr.IsChecksumValid);
+                rr = new ReaderResponse(fullResponse);
+                Assert.AreEqual(item.expectedChecksum, rr.Checksum.ToArray());
+                Assert.IsTrue(rr.IsChecksumValid);
+
+                // Invalid checksum
+                fullResponse[fullResponse.Length - 2] ^= 1;
+                fullResponse[fullResponse.Length - 1] ^= 1;
+                rr = new ReaderResponse(fullResponse, ignoreChecksum: true);
+                Assert.IsFalse(rr.IsChecksumValid);
+                Assert.Throws<DataException>(() => 
+                {
+                    new ReaderResponse(fullResponse);
+                });
+            }
+        }
+
+        [Test]
+        public void TestFullResponse()
+        {
+            for (int i = 0; i < 260; ++i)
+            {
+                byte[] data = new byte[i];
+                for (int j = 3; j < data.Length - 2; ++j)
+                {
+                    data[j] = (byte)j.GetHashCode(); // Just filling it with known stuff
+                }
+                if (data.Length >= 3)
+                {
+                    data[0] = (byte)CommunicationType.Operation;
+                    data[2] = (byte)(i - 3);
+                }
+
+                if (i >= 5 && i <= 258)
+                {
+                    ReaderResponse rr = new ReaderResponse(data, ignoreChecksum: true);
+                    Assert.AreEqual(data, rr.FullResponse.ToArray());
+                } 
+                else
+                {
+                    Assert.Throws<ArgumentException>(() =>
+                    {
+                        new ReaderResponse(data, ignoreChecksum: true);
+                    });
+                }
+            }
+        }
+    }
+}
